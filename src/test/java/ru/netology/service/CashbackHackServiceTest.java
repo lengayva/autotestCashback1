4dc17da3c476e9f2d03e8ab6a0dff2c5c7431f3e
Elena Kovylyaeva <lengayva@yandex.ru>
2024-02-11 21:42:00 +0500

    testNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,8 @@
+package ru.netology.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
+class CashbackHackServiceTest {
+    @org.testng.
+
+}
